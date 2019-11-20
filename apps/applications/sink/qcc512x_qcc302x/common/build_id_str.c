d2bc33f178f9b43eb161dbc7318977e762557ec0
luofengjun <luofengjun@vcyber.com>
2019-11-20 09:38:30 +0800

    trace all files in apps directory

---
+++ apps/applications/sink/qcc512x_qcc302x/common/build_id_str.c
@@ -0,0 +1,6 @@
+#include <hydra_types.h>
+
+/** This file will be re-generated on each build */
+
+const uint8 build_id_string[] = "QTIL ADK 2019-11-18 16:12:35 @1574064755";
+const uint32 build_id_number = 1574064755UL;
